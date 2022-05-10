d7ccebd30b4f5f0e7ee2c77c7e4ff2579a356579
hades24 <94883043+hades24@users.noreply.github.com>
2022-05-10 15:20:27 +0000

    06

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -1,6 +1,21 @@
+largest = None
+smallest = None
+a=[]
+n=10
+
 while True:
-  a=input("enter a number")
-  if a == "done":
-    break
-  print(a)
-print("done!")
+    for i in range(0,n):
+        l=input("enter the number \n")
+        a.append(l)
+        if l == "done":
+            break
+        if a[i]>a[i+1]:
+            largest=i
+        else:
+            a[i]=a[i+1]
+        if a[i]<a[i+1]:
+            smallest = i
+        else:
+            a[i]=a[i+1]
+    print("largest = ",largest)
+    print("smallest : ",smallest)
