9cf4581fc1b5b7c485fb552acc9ae196d9f0f292
hades24 <94883043+hades24@users.noreply.github.com>
2022-05-13 17:08:41 +0000

    08

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,6 @@
-# Files
-
-filename = "dataset/mbox-short.txt"
+filename="dataset/mbox-short.txt"
+fhand=open(filename)
+num=""
+for line in fhand:
+  if line.startswith("X-DSPAM-Confidence:"):
+    print(line)
