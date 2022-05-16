f0fb47918cd9025e0d7e274f984989506a5af82e
hades24 <94883043+hades24@users.noreply.github.com>
2022-05-16 03:41:11 +0000

    ammended 08

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,6 +1,12 @@
 filename="dataset/mbox-short.txt"
-fhand=open(filename)
+try:
+  fhand=open(filename)
+except: 
+  print("FILE NOT FOUND")
 num=""
+count = 0
 for line in fhand:
+  line = line.rstrip()
   if line.startswith("X-DSPAM-Confidence:"):
-    print(line)
+    count = count + 1
+    print(count, line)
